c454b75f43a78e98297da0ca0a1b6fa2a9732d76
Anderson Lorena <anderson_datadyne@hotmail.com>
2024-01-27 08:32:51 -0300

    wip: refactored some domain good practices.

---
+++ aspnet-core/src/AL.Abp.Domain/DomainConsts.cs
@@ -0,0 +1,13 @@
+﻿namespace AL.Abp
+{
+    public static class DomainConsts
+    {
+        public const int CmsPageNameMaxSize = 128;
+
+        public const int CmsPageHtmlContentMaxSize = 4096;
+
+        public const string CmsPageNameValidationError = "Name has to be between 1 and 128 chars!";
+
+        public const string CmsPageHtmlContentValidationError = "Name has to be between 1 and 4096 chars!";
+    }
+}
--- aspnet-core/src/AL.Abp.Domain/Entities/CmsPage.cs
+++ aspnet-core/src/AL.Abp.Domain/Entities/CmsPage.cs
@@ -1,13 +1,31 @@
 ﻿using System;
-using Volo.Abp.Domain.Entities;
+using Volo.Abp;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace AL.Abp.Entities
 {
     public class CmsPage : AuditedAggregateRoot<Guid>
     {
-        public string Name { get; set; }
+        public string Name { get; protected set; }
 
-        public string HtmlContent { get; set; }
+        public string HtmlContent { get; protected set; }
+
+        public CmsPage(string name, string htmlContent)
+            : base()
+        {
+            Name = name;
+            HtmlContent = htmlContent;
+
+            Validade();
+        }
+
+        private void Validade()
+        {
+            if (string.IsNullOrEmpty(Name) || Name.Length > DomainConsts.CmsPageNameMaxSize)
+                throw new BusinessException(DomainConsts.CmsPageNameValidationError);
+
+            if (string.IsNullOrEmpty(HtmlContent) || HtmlContent.Length > DomainConsts.CmsPageNameMaxSize)
+                throw new BusinessException(DomainConsts.CmsPageHtmlContentValidationError);
+        }
     }
 }
